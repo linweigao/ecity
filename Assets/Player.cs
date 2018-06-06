e29b46d31916a4a69eb5380d5a30858ef08336d2
Gao <jogao1983@gmail.com>
2018-06-05 20:23:16 -0700

    add keyboard controller

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     private bool go = false;
+    private bool turnRight = false;
+    private bool turnLeft = false;
 
 
 	// Use this for initialization
@@ -12,10 +14,55 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        var player = this.gameObject;
+        if (Input.touchCount > 0) {
+            // Always reset indicator if there is a new touch.
+            this.go = false;
+            this.turnRight = false;
+            this.turnLeft = false;
+
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                if (touch.position.x < 50) {
+                    // Turn right
+                    this.turnRight = true;
+                }
+}
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            this.go = true;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            this.go = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            this.turnRight = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            this.turnRight = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.turnLeft = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            this.turnLeft = false;
+        }
+
+        if(this.turnRight) {
+            player.transform.Rotate(0, 1f, 0);
+        }
+
+        if (this.turnLeft)
+        {
+            player.transform.Rotate(0, -1f, 0);
+        }
+
         if (this.go) {
-            var player = this.gameObject;
-            var position = player.transform.position;
-            player.transform.position = new Vector3(position.x, position.y, position.z + 0.1f);
+            player.transform.Translate(0, 0, 0.1f);
         }
 
 	}
