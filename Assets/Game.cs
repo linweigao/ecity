3e427f03f34d2aff853655aa6e2e62bb60e330de
Gao <jogao1983@gmail.com>
2018-06-05 19:57:40 -0700

    Camera and Go

---
+++ Assets/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    void OnGUI()
+    {
+        //GUI.Button(new Rect(25, 25, 100, 30), "I am a Fixed Layout Button");
+
+        // Automatic Layout
+        //GUILayout.Button("I am an Automatic Layout Button");
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private bool go = false;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.go) {
+            var player = this.gameObject;
+            var position = player.transform.position;
+            player.transform.position = new Vector3(position.x, position.y, position.z + 0.1f);
+        }
+
+	}
+
+    void OnGUI()
+    {
+        string text = this.go ? "Stop" : "Go";
+
+        // Fixed Layout
+        if (GUI.Button(new Rect(Screen.width - 50 , Screen.height - 25, 40, 20), text)) {
+            this.go = !this.go;
+        }
+    }
+}
