b00d024f276b2cedff9a116a36c3a72de97c7b4f
kchen5 <kchen5@ebay.com>
2018-06-08 10:59:58 -0700

    add prefab to this branch (#14)

--- Assets/TextUpdate.cs
+++ Assets/TextUpdate.cs
@@ -4,44 +4,76 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextUpdate : MonoBehaviour {
+public class TextUpdate : MonoBehaviour
+{
 
-    private long dynamicValue = 20L;
+    public long baseValue = 10L;
     private System.Random random = new System.Random();
     public Text mytext = null;
     public Player player = null;
+    public float refreshInterval = 1f;
+    public bool isDaynamic = true;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine("GetCurrentCount");
+    // Use this for initialization
+    void Start()
+    {
+        if (isDaynamic)
+        {
+            StartCoroutine("GetCurrentCount");
+        }
+        else
+        {
+            mytext.text = "5000+";
+            mytext.color = new Color(0.5377f, 0.0684f, 0.0684f, 1);
+            mytext.transform.rotation = player.transform.rotation;
+        }
     }
-	 
+
+    private void Update()
+    {
+        if (!isDaynamic)
+        {
+            mytext.transform.rotation = player.transform.rotation;
+        }
+    }
     IEnumerator GetCurrentCount()
     {
-        for (; ; )
+        for (; baseValue < 5000;)
         {
             int tmp = random.Next(10);
-            if(tmp % 5 == 1)
+            if (tmp % 5 < 2)
             {
                 tmp *= -1;
             }
-            dynamicValue += tmp;
-            mytext.text = string.Format("{0}", dynamicValue);
-            if(dynamicValue > 100)
+            baseValue += tmp;
+            mytext.text = string.Format("{0}", baseValue < 0 ? 0 : baseValue);
+            Color textColor;
+            if (baseValue < 100)
             {
-                mytext.color = Color.green;
+                textColor = Color.yellow;
             }
-            if(dynamicValue > 200)
+            else if (baseValue < 200)
             {
-                mytext.color = Color.red;
+                textColor = Color.green;
             }
-            if(dynamicValue > 500)
+            else if (baseValue < 500)
             {
-                mytext.color = Color.black;
+                textColor = Color.blue;
             }
+            else
+            {
+                textColor = Color.red;
+            }
+            textColor.a = 0.7f;
+            mytext.color = textColor;
             mytext.transform.rotation = player.transform.rotation;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(refreshInterval + tmp / 10);
         }
+
+        mytext.text = "5000+";
+        mytext.color = new Color(0.5377f, 0.0684f, 0.0684f, 1);
+        mytext.transform.rotation = player.transform.rotation;
+        yield return new WaitForSeconds(refreshInterval * 10);
     }
 
 }
